e9ef0f0beae262aedd28dcbd8978abd88faab353
elmiramm <elmira.m.m@yandex.ru>
2022-10-30 22:34:40 +0300

    jqwery, slick, slider

--- js/script.js
+++ js/script.js
@@ -55,4 +55,16 @@ $(function () {
 	})
 
 
+	// Reviews  https://kenwheeler.github.io/slick/
+
+	let slider = $("#reviewsSlider");
+
+	slider.slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		dots: true,
+		arrows: false
+	});
 });
