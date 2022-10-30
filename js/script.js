cbaf3dc4060e0a32e20fae2da359d1519427cbc1
elmiramm <elmira.m.m@yandex.ru>
2022-10-30 21:48:49 +0300

    jqwery, nav-toggle

--- js/script.js
+++ js/script.js
@@ -4,21 +4,30 @@ $(function () {
 	let intro = $("#intro");
 	let introH = intro.innerHeight();
 	let scrollPos = $(window).scrollTop();
+	let nav = $("#nav");
+	let navToggle = $('#navToggle');
+
+	checkScroll(scrollPos, introH);
 
 	// Fixed header
 
-	$(window).on("scroll load resize", function () {
+	$(window).on("scroll resize", function () {
 		scrollPos = $(this).scrollTop();
-
 		introH = intro.innerHeight();
+
+		checkScroll(scrollPos, introH);
+
+	})
+
+	function checkScroll(scrollPos, introH) {
 
 		if (scrollPos > introH) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
 		}
+	}
 
-	})
 
 	// smooth Scroll
 
@@ -28,10 +37,21 @@ $(function () {
 		let elementId = $(this).data("scroll");
 		let elementOffset = $(elementId).offset().top;
 
+		nav.removeClass("show");
+
 		$("html, body").animate({
 			scrollTop: elementOffset - 70
 		}, 700)
 
+	})
+
+	// navToggle 
+
+
+	navToggle.on("click", function (event) {
+		event.preventDefault();
+
+		nav.toggleClass("show");
 	})
 
 
